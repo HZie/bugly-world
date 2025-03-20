1463ed09a86de519359f777659c2f51f64ff6fb7
HZie <jiyeon.han347@gmail.com>
2025-03-20 17:10:10 +0900

    cebook

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-import backgroundImage from "./assets/urdy.png";
+//import styles from "./App.module.css";
+import BugFound from "./BugFound";
 
 function App() {
-  // 시간 표시
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    // 1초마다 현재 시간 업데이트
-    const timer = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  // 날짜 포맷: YYYY-MM-DD
-  const formattedDate = time.toISOString().split("T")[0];
-
-  // 시간 포맷: HH:MM:ss
-  const timeParts = time.toTimeString().split(" ")[0].split(":");
-  const hoursMinutes = `${timeParts[0]}:${timeParts[1]}`; // HH:MM
-  const seconds = timeParts[2]; // ss
-
-  return (
-    <div
-      className={styles.mobile}
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
-      <div className={styles.overlay}>
-        <div className={styles.warning}>밀레니엄 버그 발견</div>
-        <div className={styles.date}>{formattedDate}</div>
-        <div className={styles.time}>
-          <span className={styles.hoursMinutes}>{hoursMinutes}</span>
-          <span className={styles.seconds}> {seconds}</span>
-        </div>
-      </div>
-      <div className={styles.bug}>
-        <span className={styles.bugTitle}>VIRUS</span>
-        <span className={styles.bugName}>URDY</span>
-      </div>
-    </div>
-  );
+  return <BugFound />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import BugFound from "./BugFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
   <App />
+  //<BugFound />
   //</React.StrictMode>
 );
